3e016c596d77a3253672cd8b38b7acd3da6beae8
Marcus Camaroni <marcus.camaroni@versent.com.au>
2026-09-14 08:15:27 +1000

    fix(darwin): pin nix-darwin to nixpkgs-unstable
    - nixos unstable checks are redundant on darwin configs

--- flake.nix
+++ flake.nix
@@ -2,21 +2,26 @@
   description = "My system configuration";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # NixOS hosts
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # Mac
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    import-tree.url = "github:denful/import-tree";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
     neovim-config = {
       url = "github:Marcus441/neovim.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    import-tree.url = "github:denful/import-tree";
   };
 
   outputs = inputs:
